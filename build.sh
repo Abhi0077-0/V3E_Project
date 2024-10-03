8fdf67736240d77e5bd4a96cf2f2aaea55613b44
Abhi0077-0 <abhinavtuplondhe77@gmail.com>
2024-10-03 14:35:13 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
 
+# Use Rustup to install Rust toolchain
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y --default-toolchain stable
+
+# Set environment variables to ensure Rust is available in the PATH
+source $HOME/.cargo/env
+
 # Install Python dependencies
 pip install -r requirements.txt
 
-# Install Rust toolchain
-curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
-source $HOME/.cargo/env
-
-# Install Node.js dependencies and build the React app
+# Build React frontend
 npm install --prefix task-manager-frontend
 npm run build --prefix task-manager-frontend
+
+chmod +x build.sh
