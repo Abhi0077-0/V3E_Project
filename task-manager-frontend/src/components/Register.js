113fcc89e7df625c71bd5319df516e1057645525
Abhi0077-0 <abhinavtuplondhe77@gmail.com>
2024-10-03 17:06:49 +0530

    Update Register.js

--- task-manager-frontend/src/components/Register.js
+++ task-manager-frontend/src/components/Register.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import axios from 'axios';
 import { useNavigate } from 'react-router-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faUser, faEnvelope, faLock } from '@fortawesome/free-solid-svg-icons';
